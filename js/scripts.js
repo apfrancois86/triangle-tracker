3311bfa0456c0badf45755a30d5031c64a3649fc
Alex Francois and Erica Nafziger <erica@>
2016-10-20 15:48:05 -0700

    add all business logic to triangle function

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,15 @@
 //business logic
 var triangle = function(side1,side2,side3){
-  if (side1 === side2 && side2 === side3) {
+  if (side1 === side2 && side2 === side3 && side1 + side2 >= side3 && side2 + side3 >= side1 && side1 + side3 >= side2) {
   return "equilateral";
-} else if (side1 === side2 || side2 === side3 || side1===side3) {
+} else if (side1 === side2 && side1 + side2 >= side3 && side2 + side3 >= side1 && side1 + side3 >= side2 || side2 === side3 && side1 + side2 >= side3 && side2 + side3 >= side1 && side1 + side3 >= side2 || side1===side3 && side1 + side2 >= side3 && side2 + side3 >= side1 && side1 + side3 >= side2) {
   return "isosceles";
+} else if (side1!=side2 && side2!=side3 && side1 + side2 >= side3 && side2 + side3 >= side1 && side1 + side3 >= side2)  {
+  return "scalene";
+} else {
+  return "not a triangle";
 }
+
 }
 
 /*var isosceles = function(side1,side2,side3){
